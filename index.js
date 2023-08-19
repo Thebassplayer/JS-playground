45556354a18055af031942870d76ab490657586d
Thebassplayer <ryhsu2@gmail.com>
2023-08-19 15:04:47 -0300

    1 file modified

--- index.js
+++ index.js
@@ -1,32 +1,10 @@
-/**
- * Convert HTML Entities
-Convert the characters &, <, >, " (double quote), and ' (apostrophe), in a string to their corresponding HTML entities.
- */
-
-function convertHTML(str) {
-  function characterChanger(character) {
-    switch (character) {
-      case '&':
-        return '&amp;';
-      case '<':
-        return '&lt;';
-      case '>':
-        return '&gt;';
-      case '"':
-        return '&quot;';
-      case "'":
-        return '&apos;';
-      default:
-        return character;
-    }
-  }
-
-  return str
-    .split('')
-    .map((char) => characterChanger(char))
-    .join('');
+function fetchDataWithCallback(callback) {
+  setTimeout(() => {
+    const data = "Async data";
+    callback(data);
+  }, 1000);
 }
 
-const result = convertHTML('Dolce & Gabbana');
-
-console.log(result);
+fetchDataWithCallback((data) => {
+  console.log(data);
+});
